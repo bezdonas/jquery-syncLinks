61decd8d83763206d050c1ac54216afd250927ef
Aliev Ramil <bezdonas@gmail.com>
2013-05-22 19:15:28 +0400

    INIT

---
+++ script.js
@@ -0,0 +1,28 @@
+(function($) {
+
+	// Синхронизирует ховер у всех ссылок ведущих на одинаковую страницу внутри вызывающего контейнера
+	// Посредством класса hover (нужно прописать в цсс)
+	$.fn.syncLinks = function(hoverClass) {
+
+		var cont = $(this),
+			hoverClass = hoverClass ? hoverClass : 'hover';
+
+		cont.find('a').each(function() {
+
+			var elem = $(this),
+				href = elem.attr('href'),
+				brothers = cont.find('a[href="' + href + '"]');
+
+			elem
+				.on('mouseover', cont, function() {
+					brothers.addClass(hoverClass);
+				})
+				.on('mouseleave', cont, function() {
+					brothers.removeClass(hoverClass);
+				});
+
+		});
+
+	}
+
+})(jQuery);
